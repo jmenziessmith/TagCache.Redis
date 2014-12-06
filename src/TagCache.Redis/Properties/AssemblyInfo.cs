e8eff1a193d1c199e2ffcf03b9c536fe316948dd
jmenziessmith <github@menzies-smith.co.uk>
2014-12-06 02:05:50 +0000

    fix assmbly info

--- src/TagCache.Redis/Properties/AssemblyInfo.cs
+++ src/TagCache.Redis/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Architecture.Patterns.Cache.RedisCache")]
+[assembly: AssemblyTitle("TagCache.Redis")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Architecture.Patterns.Cache.RedisCache")]
+[assembly: AssemblyProduct("TagCache.Redis")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
