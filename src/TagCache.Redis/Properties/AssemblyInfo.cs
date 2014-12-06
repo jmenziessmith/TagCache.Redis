96a2e5c54e9738e07dc5640d8f8bf42b66e85967
jmenziessmith <github@menzies-smith.co.uk>
2014-12-06 13:57:32 +0000

    Create nuget package

--- src/TagCache.Redis/Properties/AssemblyInfo.cs
+++ src/TagCache.Redis/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TagCache.Redis")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription(".Net Redis Cache with support for tagging")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Jon Menzies-Smith")]
 [assembly: AssemblyProduct("TagCache.Redis")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.9")]
+[assembly: AssemblyFileVersion("0.0.0.9")]
---
+++ src/TagCache.Redis/nuget-create.bat
@@ -0,0 +1,2 @@
+C:\Windows\Microsoft.Net\Framework64\v4.0.30319\msbuild.exe TagCache.Redis.csproj /p:Configuration=Release
+nuget pack TagCache.Redis.csproj -IncludeReferencedProjects -Prop Configuration=Release -OutputDirectory ../../lib/nuget
