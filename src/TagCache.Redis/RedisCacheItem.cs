ed9671db9cd3de0bd172f4515f9b871407d480f8
Fabian Nicollier <fabian.nicollier@symbioticsgroup.com>
2014-12-12 16:38:58 +0100

    Added Protocol Buffers serializer and tests.

--- src/TagCache.Redis/RedisCacheItem.cs
+++ src/TagCache.Redis/RedisCacheItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using ProtoBuf;
 
 namespace TagCache.Redis
 {
@@ -13,14 +14,27 @@ namespace TagCache.Redis
     [Serializable]
     public class RedisCacheItem : IRedisCacheItem
     {
-        public string Key { get; set; }
-        public List<string> Tags { get; set; }
-        public DateTime Expires { get; set; }
+        public virtual string Key { get; set; }
+        
+        public virtual List<string> Tags { get; set; }
+        
+        public virtual DateTime Expires { get; set; }
     }
 
     [Serializable]
+    [ProtoContract]
     public class RedisCacheItem<T> : RedisCacheItem
     {
+        [ProtoMember(1)]
+        public override string Key { get; set; }
+
+        [ProtoMember(2)]
+        public override List<string> Tags { get; set; }
+
+        [ProtoMember(3)]
+        public override DateTime Expires { get; set; }
+
+        [ProtoMember(4)]
         public T Value { get; set; }
     }
 }
---
+++ src/TagCache.Redis/Serialization/ProtoBufSerializationProvider.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using ProtoBuf;
+using StackExchange.Redis;
+using TagCache.Redis.Interfaces;
+
+namespace TagCache.Redis.Serialization
+{
+    public class ProtoBufSerializationProvider : ISerializationProvider
+    {
+        public T Deserialize<T>(RedisValue value) where T : class
+        {
+            using (var memoryStream = new MemoryStream(value))
+            {
+                return Serializer.Deserialize<T>(memoryStream);
+            }
+        }
+
+        public RedisValue Serialize<T>(T value) where T : class
+        {
+            using (var mStream = new MemoryStream())
+            {
+                Serializer.Serialize(mStream, value);
+                var bytes = mStream.ToArray();
+                return bytes;
+            }
+        }
+    }
+}
