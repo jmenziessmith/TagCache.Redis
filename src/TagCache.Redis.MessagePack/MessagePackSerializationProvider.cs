c61e3ea1b0eba875f86c2fb18fced22e18cbcf7e
Fabian Nicollier <fabian.nicollier@symbioticsgroup.com>
2014-12-18 08:05:49 +0100

    Added MessagePack serializer.

---
+++ src/TagCache.Redis.MessagePack/MessagePackSerializationProvider.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using StackExchange.Redis;
+using TagCache.Redis.Interfaces;
+using MsgPack.Serialization;
+
+namespace TagCache.Redis.MessagePack
+{
+    public class MessagePackSerializationProvider : ISerializationProvider
+    {
+        public T Deserialize<T>(RedisValue value) where T : class
+        {
+            using (var stream = new MemoryStream(value))
+            {
+                return SerializationContext.Default.GetSerializer<T>().Unpack(stream);
+            }
+        }
+
+        public RedisValue Serialize<T>(T value) where T : class
+        {
+            using (var stream = new MemoryStream())
+            {
+                SerializationContext.Default.GetSerializer<T>().Pack(stream, value);
+                return stream.ToArray();
+            }
+        }
+    }
+}
