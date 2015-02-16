00d019b333940f9af05770168108e1f9b261a0c6
Fabian Nicollier <fabian.nicollier@symbioticsgroup.com>
2015-02-16 11:26:59 +0100

    Cleanup. Updated version number. Made new build and packages. Added file that was missing from previous commit.

---
+++ src/TagCache.Redis.ProtoBuf/ISerializerBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TagCache.Redis.ProtoBuf
+{
+    /// <summary>
+    /// An interface for Protobuf serializer builders.
+    /// </summary>
+    public interface ISerializerBuilder
+    {
+        /// <summary>
+        /// Builds the serializer for the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type.</typeparam>
+        void Build<T>();
+    }
+}
